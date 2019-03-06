f5a8199ee031a14b8a7584185e6a2a95ebfc0a4a
KieranVieira <kieranvieira@live.com>
2019-03-06 16:52:23 -0500

    Added tests for display

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,8 @@
-// Test away!
+import React from 'react';
+import { render } from 'react-testing-library';
+
+import Controls from './Controls';
+
+describe('<Controls/>', () => {
+
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,30 @@
-// Test away!
+import React from 'react';
+import { render } from 'react-testing-library';
+
+import Display from './Display';
+
+describe('<Display/>', () => {
+    it('Should render closed if closed is true', () => {
+        const { getByText } = render(<Display closed={true}/>);
+
+        expect(getByText(/closed/i)).toBeDefined();
+    });
+
+    it('Should render open if closed is false', () => {
+        const { getByText } = render(<Display closed={false}/>);
+
+        expect(getByText(/open/i)).toBeDefined();
+    });
+
+    it('Should render locked if locked is true', () => {
+        const { getByText } = render(<Display locked={true} closed={true}/>);
+
+        expect(getByText(/locked/i)).toBeDefined();
+    });
+
+    it('Should render unlocked if locked is false', () => {
+        const { getByText } = render(<Display locked={false} closed={true}/>);
+
+        expect(getByText(/unlocked/i)).toBeDefined();
+    });
+});
